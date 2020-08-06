8756ce29f2f3082a4c88d9a86c52a4e8d10aaf95
1023299082 <yw3367@columbia.edu>
2020-08-06 11:34:21 -0400

    "first commit"

--- app/src/main/java/com/example/smartdressing/Camera.java
+++ app/src/main/java/com/example/smartdressing/Camera.java
@@ -7,13 +7,16 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Camera extends AppCompatActivity {
+    private ImageView imageview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         dispatchTakePictureIntent();
+        imageview = findViewById(R.id.camera_preview);
     }
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -24,12 +27,12 @@ public class Camera extends AppCompatActivity {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            imageView.setImageBitmap(imageBitmap);
-//        }
-//    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageview.setImageBitmap(imageBitmap);
+        }
+    }
 }
