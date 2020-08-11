e994749f8fbc1092c0287964c471cee89296cc6f
woshihaozhu <358975248@qq.com>
2020-08-11 22:17:43 +0800

    修改了注册文件的文件名，在其背后添加了activity

--- app/src/main/java/com/example/smartdressing/ui/RegisterProgressActivity.java
+++ app/src/main/java/com/example/smartdressing/ui/RegisterProgressActivity.java
@@ -2,7 +2,6 @@ package com.example.smartdressing.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -12,7 +11,6 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.smartdressing.R;
 import com.example.smartdressing.utils.RegexUtils;
@@ -29,7 +27,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class RegisterProgress extends AppCompatActivity {
+public class RegisterProgressActivity extends AppCompatActivity {
     //界面控件
     private Button mBtnRegisterNext;
     private EditText mEditTextTextEmailAddress;
@@ -171,16 +169,16 @@ public class RegisterProgress extends AppCompatActivity {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    ToastUtils.showShort(RegisterProgress.this, "注册成功");
+                    ToastUtils.showShort(RegisterProgressActivity.this, "注册成功");
                 }
             });
             return true;
-        }catch (Exception e){
+        }catch (IOException | JSONException e){
             e.printStackTrace();
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    ToastUtils.showShort(RegisterProgress.this, "注册失败");
+                    ToastUtils.showShort(RegisterProgressActivity.this, "注册失败");
                 }
             });
         }
